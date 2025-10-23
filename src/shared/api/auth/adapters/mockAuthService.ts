aeeaaaad8ea471204324e38f64c24abc76dec87e
Vitali Maisiayonak <moiseenokv@gmail.com>
2025-10-23 12:32:06 +0300

    chore(env): node engines/.nvmrc, tsconfig tweaks, husky hooks

--- src/shared/api/auth/adapters/mockAuthService.ts
+++ src/shared/api/auth/adapters/mockAuthService.ts
@@ -1,14 +1,10 @@
 import type { AuthService, LoginPayload, RegisterPayload } from '../port';
 
-let _isAuth = false;
-
 export const mockAuthService: AuthService = {
   async login(_data: LoginPayload) {
-    _isAuth = true;
     return { token: 'mock-token' };
   },
   async register(_data: RegisterPayload) {
-    _isAuth = true;
     return;
   },
 };
